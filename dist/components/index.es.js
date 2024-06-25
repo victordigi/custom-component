76ee43705325f928f0f43bfcfe6c5ce1e59ac09b
Victor HONG <victor81528@hotmail.com>
2024-06-25 17:27:41 +0800

    no message

--- dist/components/index.es.js
+++ dist/components/index.es.js
@@ -1,5 +1,5 @@
-import { openBlock as o, createElementBlock as e, toDisplayString as i } from "vue";
-const r = {
+import { openBlock as o, createElementBlock as e, toDisplayString as r } from "vue";
+const s = {
   __name: "DigiButton",
   props: {
     msg: String
@@ -8,10 +8,9 @@ const r = {
     const n = () => {
       alert("啊啊啊啊啊啊");
     };
-    return (c, a) => (o(), e("button", { onClick: n }, i(t.msg), 1));
+    return (a, c) => (o(), e("button", { onClick: n }, r(t.msg), 1));
   }
 };
-typeof window < "u" && window.Vue && window.Vue.component("DigiButton", r);
 export {
-  r as DigiButton
+  s as DigiButton
 };
--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,3 @@ import DigiButton from "./DigiButton.vue"
 export {
   DigiButton,
 }
-
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.component('DigiButton', DigiButton)
-}
