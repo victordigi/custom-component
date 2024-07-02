7ecf27e4275be3ac3cbb2084d0e3e4e3201eca12
Victor HONG <victor81528@hotmail.com>
2024-07-02 17:25:13 +0800

    no message

--- dist/components/index.es.js
+++ dist/components/index.es.js
@@ -1,24 +1,10 @@
-import { openBlock as e, createElementBlock as c, toDisplayString as s } from "vue";
-const a = {
-  __name: "DigiButton",
-  props: {
-    msg: String
-  },
-  setup(t) {
-    const o = () => {
-      alert("啊啊啊啊啊啊");
-    };
-    return (n, l) => (e(), c("button", { onClick: o }, s(t.msg), 1));
-  }
-}, r = [
-  a
-], i = function(t, o = {}) {
-  r.forEach((n) => {
-    t.component(n.name, n);
-  });
-}, p = {
-  install: i
-};
-export {
-  p as default
-};
+var o = (i, t) => () => (t || i((t = { exports: {} }).exports, t), t.exports);
+var u = o((p, n) => {
+  let e = require("./DigiButton");
+  n.exports = {
+    install: function(i, t) {
+      i.component("DigiButton", e);
+    }
+  };
+});
+export default u();
--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,23 @@
-import DigiButton from "./DigiButton.vue"
+// import DigiButton from "./DigiButton.vue"
 
 
-const Components = [
-  DigiButton
-]
-const install = function (Vue, opts = {}) {
-  Components.forEach(component => {
-    Vue.component(component.name, component)
-  })
- }
+// const Components = [
+//   DigiButton
+// ]
+// const install = function (App) {
+//   Components.forEach(component => {
+//     App.component(component.name, component)
+//   })
+//  }
 
-export default {
-  install,
-}
+// export default {
+//   install,
+// }
+
+let DigiButton = require('./DigiButton');
+
+module.exports = {
+    install: function (Vue, options) {
+        Vue.component('DigiButton', DigiButton);
+    }
+};
