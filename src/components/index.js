7618a8aa4bb623eadd1a646c6b4ed1bc1d8d6e72
Victor HONG <victor81528@hotmail.com>
2024-07-03 17:26:15 +0800

    no message

--- src/components/index.js
+++ src/components/index.js
@@ -1,23 +1,3 @@
 import DigiButton from "./DigiButton.vue"
 
-export {
-  DigiButton
-}
-// const Components = [
-//   DigiButton
-// ]
-// const install = function (App) {
-//   Components.forEach(component => {
-//     App.component(component.name, component)
-//   })
-//  }
-
-// export default {
-//   install,
-// }
-
-// module.exports = {
-//     install: function (Vue, options) {
-//         Vue.component('DigiButton', DigiButton);
-//     }
-// };
+export { DigiButton };
