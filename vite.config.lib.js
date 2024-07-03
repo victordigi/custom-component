bddca881d48bb6a1a7946e042fa0e59f6c08e39d
Victor HONG <victor81528@hotmail.com>
2024-07-03 16:33:50 +0800

    no message

--- vite.config.lib.js
+++ vite.config.lib.js
@@ -10,7 +10,7 @@ export default defineConfig({
     outDir: 'dist',
     lib: {
       entry: 'src/components/index.js',
-      name: 'digi-components',
+      name: 'DigiComponents',
       // formats: ['umd', 'es', 'cjs'],
       fileName: (format) => `index.${format}.js`
     },
